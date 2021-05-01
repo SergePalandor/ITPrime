c5616b3fce29ee826c21e0040d04128c858cddb0
Serge Palandor <serge@palandor.ru>
2021-05-01 19:41:11 +0300

    Улучшен механизм работы с числами. Добавлены и улучшены тесты. Сам разрабатываемый метод не менялся, результат тот же.

--- LuckyTickets/NumberX.cs
+++ LuckyTickets/NumberX.cs
@@ -13,17 +13,17 @@ namespace LuckyTickets
 		/// <summary>
 		/// Хранимое значение.
 		/// </summary>
-		private readonly string _value;
+		private string _value;
 
 		/// <summary>
 		/// Количество цифр в числе.
 		/// </summary>
-		public int DigitsCount { get; }
+		public int DigitsCount { get; private set; }
 
 		/// <summary>
-		/// Максимальный порядок в числе (0 - 'единицы', 1 - 'десятки', 2 - 'сотни' и т.д.).
+		/// Максимальный разряд в числе (0 - 'единицы', 1 - 'десятки', 2 - 'сотни' и т.д.).
 		/// </summary>
-		public int MaxRank { get; }
+		public int MaxRank { get; private set; }
 
 		/// <summary>
 		/// Является ли число отрицательным.
@@ -44,20 +44,21 @@ namespace LuckyTickets
 		/// Конструктор.
 		/// </summary>
 		/// <param name="number">Число в десятичной системе счисления.</param>
-		public NumberX(int number, IReadOnlyList<char> usingDigits)
+		public NumberX(long number, IReadOnlyList<char> usingDigits)
 		{
-			if (usingDigits == null)
+			this.UsingDigits = new List<char>(usingDigits);
+
+			if (this.UsingDigits == null)
 				throw new ArgumentNullException(nameof(usingDigits));
 
-			if (usingDigits.Count < 1)
+			if (this.UsingDigits.Count < 1)
 				throw new ArgumentException("Для определения базы системы счисления необходимо передать список символов, используемых в качестве цифр.", nameof(usingDigits));
 
-			this.UsingDigits = usingDigits;
 			this.NumberBase = this.UsingDigits.Count;
 
 			if (number == 0)
 			{
-				_value = usingDigits[0].ToString();
+				_value = this.UsingDigits[0].ToString();
 			}
 			else
 			{
@@ -72,11 +73,11 @@ namespace LuckyTickets
 
 				while (number > 0)
 				{
-					var base10DigitValue = number % numberBase;
-					if (usingDigits.Count <= base10DigitValue)
+					var base10DigitValue = (int)(number % numberBase);
+					if (this.UsingDigits.Count <= base10DigitValue)
 						throw new InvalidCastException($"В системе счисления с базой '{this.NumberBase}' не задана цифра, описывающая десятичное значение '{base10DigitValue}'.");
 
-					sb.Insert(0, usingDigits[base10DigitValue]);
+					sb.Insert(0, this.UsingDigits[base10DigitValue]);
 					number /= numberBase;
 				}
 
@@ -96,13 +97,14 @@ namespace LuckyTickets
 			if (string.IsNullOrWhiteSpace(value) || value == "-")
 				throw new ArgumentException(message: "Требуется задать корректное значение числа.", nameof(value));
 
-			if (usingDigits == null)
+			this.UsingDigits = new List<char>(usingDigits);
+
+			if (this.UsingDigits == null)
 				throw new ArgumentNullException(nameof(usingDigits));
 
-			if (usingDigits.Count < 1)
+			if (this.UsingDigits.Count < 1)
 				throw new ArgumentException("Для определения базы системы счисления необходимо передать список символов, используемых в качестве цифр.", nameof(usingDigits));
 
-			this.UsingDigits = usingDigits;
 			this.NumberBase = this.UsingDigits.Count;
 
 			if (value[0] == '-')
@@ -113,7 +115,7 @@ namespace LuckyTickets
 
 			foreach (var digit in value)
 			{
-				if (!usingDigits.Contains(digit))
+				if (!this.UsingDigits.Contains(digit))
 					throw new ArgumentException($"В системе счисления с базой '{this.NumberBase}' не используется цифра '{digit}'.");
 			}
 
@@ -123,9 +125,9 @@ namespace LuckyTickets
 		}
 
 		/// <summary>
-		/// Метод получения цифры по порядку.
+		/// Метод получения цифры по её разряду.
 		/// </summary>
-		/// <param name="rank">Порядок, где 0 - 'единицы', 1 - 'десятки', 2 - 'сотни' и т.д.</param>
+		/// <param name="rank">Разряд, где 0 - 'единицы', 1 - 'десятки', 2 - 'сотни' и т.д.</param>
 		/// <returns>Искомая цифра.</returns>
 		public char GetDigit(int rank) => rank > this.MaxRank ? this.UsingDigits[0] : _value[this.MaxRank - rank];
 
@@ -135,11 +137,15 @@ namespace LuckyTickets
 		public long ToInteger()
 		{
 			var result = 0L;
-			var multiplier = 1;
+			var multiplier = 1L;
 			for (var rank = 0; rank <= this.MaxRank; rank++)
 			{
 				var base10DigitValue = this.ToInteger(rank);
-				result += base10DigitValue * multiplier;
+				var val = base10DigitValue * multiplier;
+				if (long.MaxValue - result < val)
+					throw new OverflowException($"Число '{this}' в {this.NumberBase}-ичной системе счисления нельзя привести к long, т.к. оно больше long.MaxValue.");
+
+				result += val;
 				multiplier *= this.NumberBase;
 			}
 
@@ -150,7 +156,7 @@ namespace LuckyTickets
 		}
 
 		/// <summary>
-		/// Метод перевода цифры заданного порядка (0 - 'единицы', 1 - 'десятки' и т.д.) в десятичную систему счисления.
+		/// Метод перевода цифры заданного разряда (0 - 'единицы', 1 - 'десятки' и т.д.) в десятичную систему счисления.
 		/// </summary>
 		public int ToInteger(int rank)
 		{
@@ -192,5 +198,75 @@ namespace LuckyTickets
 
 			return result;
 		}
+
+		public void IncrementAbs()
+		{
+			for (var rank = 0; rank <= this.MaxRank; rank++)
+			{
+				var base10DigitValue = this.ToInteger(rank) + 1;
+				var arr = _value.Reverse().ToArray();
+				if (base10DigitValue == this.NumberBase)
+				{
+					arr[rank] = this.UsingDigits[0];
+					arr = arr.Reverse().ToArray();
+					if (rank == this.MaxRank)
+					{
+						_value = this.UsingDigits[1].ToString() + new string(arr);
+						this.MaxRank++;
+						this.DigitsCount++;
+						break;
+					}
+					else
+					{
+						_value = new string(arr);
+					}
+				}
+				else
+				{
+					arr[rank] = this.UsingDigits[base10DigitValue];
+					arr = arr.Reverse().ToArray();
+					_value = new string(arr);
+					break;
+				}
+			}
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (obj is NumberX numberX)
+				return this == numberX;
+
+			if (obj is sbyte int8)
+				return this == new NumberX(int8, this.UsingDigits);
+			
+			if (obj is byte uint8)
+				return this == new NumberX(uint8, this.UsingDigits);
+			
+			if (obj is short int16)
+				return this == new NumberX(int16, this.UsingDigits);
+
+			if (obj is ushort uint16)
+				return this == new NumberX(uint16, this.UsingDigits);
+
+			if (obj is int int32)
+				return this == new NumberX(int32, this.UsingDigits);
+
+			if (obj is uint uint32)
+				return this == new NumberX(uint32, this.UsingDigits);
+
+			if (obj is long int64)
+				return this == new NumberX(int64, this.UsingDigits);
+
+			if(obj is ulong uint64 && uint64 <= long.MaxValue)
+				return this == new NumberX((long)uint64, this.UsingDigits);
+
+			return false;
+		}
+
+		public override int GetHashCode() => $"{this.NumberBase}_{this}".GetHashCode();
+
+		public static bool operator ==(NumberX a, NumberX b) => a.NumberBase == b.NumberBase && a.IsNegative == b.IsNegative && a._value == b._value;
+
+		public static bool operator !=(NumberX a, NumberX b) => !(a == b);
 	}
 }
--- LuckyTickets/Program.cs
+++ LuckyTickets/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace LuckyTickets
 {
@@ -22,12 +24,40 @@ namespace LuckyTickets
 			}
 			catch { }
 
-			var useLogInTest = false;
+			var useLogInTest = true;
+			try
+			{
+				useLogInTest = byte.Parse(args[2]) == 1;
+			}
+			catch { }
 
-			if (TestNumbersConvertation(useLog: useLogInTest) && TestProcessingMethod(useLog: useLogInTest))
-				_ = GetLuckyTicketsCount(usingDigits: Number13.Digits, maxRank: maxRank, checkingRanks: checkingRanks, useLog: true);
+			var useMultiThreadTest = true;
+			try
+			{
+				useMultiThreadTest = byte.Parse(args[3]) == 1;
+			}
+			catch { }
+
+			var useSingleThreadTest = false;
+			try
+			{
+				useSingleThreadTest = byte.Parse(args[4]) == 1;
+			}
+			catch { }
+
+			if
+			(
+				TestNumbersConvertation(useLog: useLogInTest)
+				&& TestDigitsSum(useLog: useLogInTest)
+				&& TestProcessingMethod(useMultiThreadTest: useMultiThreadTest, useSingleThreadTest: useSingleThreadTest, useLog: useLogInTest)
+			)
+			{
+				_ = GetLuckyTicketsCount(usingDigits: Number13.Digits, digitsCount: maxRank, checkingRanks: checkingRanks, useLog: true);
+			}
 			else
-				Console.WriteLine("Провален один или более тестов. Включите логирование для отображения подробностей.");
+			{
+				Console.WriteLine("Провален один или более тестов. Включите вывод логов в консоль для отображения подробностей.");
+			}
 
 			Console.WriteLine();
 			Console.WriteLine("Нажмите ENTER для закрытия приложения.");
@@ -38,11 +68,11 @@ namespace LuckyTickets
 		/// Метод решения задачи. Находит количество красивых чисел в заданной системе счисления, у которых сумма первых [checkingRanks] цифр слева равна сумме первых [checkingRanks] цифр справа.
 		/// </summary>
 		/// <param name="usingDigits">Цифры, используемые в системе счисления.</param>
-		/// <param name="maxRank">Количество цифр в проверяемых числах.</param>
+		/// <param name="digitsCount">Количество цифр в проверяемых числах.</param>
 		/// <param name="checkingRanks">Количество проверяемых 'разрядов' (цифр) слева и справа.</param>
 		/// <param name="useLog">Использовать ли вывод в консоль для логов.</param>
 		/// <returns>Результат решения задачи.</returns>
-		private static long GetLuckyTicketsCount(IReadOnlyList<char> usingDigits, byte maxRank, byte checkingRanks, bool useLog)
+		private static long GetLuckyTicketsCount(IReadOnlyList<char> usingDigits, byte digitsCount, byte checkingRanks, bool useLog)
 		{
 			var numberBase = usingDigits.Count;
 
@@ -50,13 +80,14 @@ namespace LuckyTickets
 			{
 				Console.WriteLine();
 				Console.WriteLine("========================================");
-				Console.WriteLine($"Начинаем решать задачу поиска красивых чисел для {maxRank}-значных чисел в {numberBase}-ичной системе счисления с проверкой по {checkingRanks} первых цифр слева и справа:");
+				Console.WriteLine();
+				Console.WriteLine($"Начинаем решать задачу поиска красивых чисел для {digitsCount}-значных чисел в {numberBase}-ичной системе счисления с проверкой по {checkingRanks} первых цифр слева и справа:");
 			}
 
-			var notCheckingRanks = maxRank - checkingRanks * 2;
+			var notCheckingRanks = digitsCount - checkingRanks * 2;
 			if (notCheckingRanks < 0)
 			{
-				Console.WriteLine($"Ошибка: Аргумент '{nameof(maxRank)}' должен быть минимум в 2 раза больше, чем аргумент '{nameof(checkingRanks)}'.");
+				Console.WriteLine($"Ошибка: Аргумент '{nameof(digitsCount)}' должен быть минимум в 2 раза больше, чем аргумент '{nameof(checkingRanks)}'.");
 				return -1;
 			}
 
@@ -75,34 +106,34 @@ namespace LuckyTickets
 			}
 
 			if (useLog)
-				Console.WriteLine($"Т.к. по заданию у нас все числа содержат {maxRank} цифр (включая ведущие нули) и мы рассматриваем все без исключения такие числа, для каждой суммы первых {checkingRanks} цифр 'слева' гарантированно будет один или несколько подходящих вариантов расположения первых {checkingRanks} цифр 'справа' с такой же суммой цифр.");
+				Console.WriteLine($"Т.к. по заданию у нас все числа содержат {digitsCount} цифр (включая ведущие нули) и мы рассматриваем все без исключения такие числа, для каждой суммы первых {checkingRanks} цифр 'слева' гарантированно будет один или несколько подходящих вариантов расположения первых {checkingRanks} цифр 'справа' с такой же суммой цифр.");
 
-			var minNumber10 = 0;
-			var minNumberX = new NumberX(minNumber10, usingDigits);
+			var numberX = new NumberX(0, usingDigits);
 			var maxNumberX = new NumberX(new string(usingDigits[numberBase - 1], checkingRanks), usingDigits);
-			var maxNumber10 = maxNumberX.ToInteger();
 
 			var differentSumsCount = maxNumberX.GetDigitsSum() + 1; // не забываем про ноль.
 
 			if (useLog)
 			{
-				Console.WriteLine($"Получается, что для решения задачи достаточно перебрать {numberBase}-ичные числа от '{minNumberX}' до '{maxNumberX}', сосчитать количество вариантов N для каждой суммы цифр 'слева' - для каждого из этих вариантов будет N вариантов искомых цифр 'справа', т.е. чтобы получить результирующее количество вариантов - нужно полученное значение возвести в квадрат.");
+				Console.WriteLine($"Получается, что для решения задачи достаточно перебрать {numberBase}-ичные числа от '{numberX}' до '{maxNumberX}', сосчитать количество вариантов N для каждой суммы цифр 'слева' - для каждого из этих вариантов будет N вариантов искомых цифр 'справа', т.е. чтобы получить результирующее количество вариантов - нужно полученное значение возвести в квадрат.");
 				Console.WriteLine($"Определили, что различных сумм цифр у нас будет {differentSumsCount}.");
 			}
 
 			var sums = new int[differentSumsCount];
 
-			for (var number10 = minNumber10; number10 <= maxNumber10; number10++)
+			maxNumberX.IncrementAbs(); // чтобы последняя проверка с самим макс. числом тоже прошла
+			while (numberX.ToString() != maxNumberX.ToString())
 			{
-				var numberX = new NumberX(number10, usingDigits);
+				//var numberX = new NumberX(number10, usingDigits);
 				var digitsSum = numberX.GetDigitsSum();
 				sums[digitsSum]++;
+				numberX.IncrementAbs();
 			}
 
 			long result = 0;
 
 			if (useLog)
-				Console.WriteLine("Сосчитали количество сочтаний цифр для каждой из сумм цифр:");
+				Console.WriteLine("Сосчитали количество сочетаний цифр для каждой из сумм цифр:");
 
 			for (var sum = 0; sum < differentSumsCount; sum++)
 			{
@@ -129,6 +160,8 @@ namespace LuckyTickets
 				Console.WriteLine($"Результат выполнения задачи: {result}");
 				Console.WriteLine("----------------------------------------");
 				Console.WriteLine();
+				Console.WriteLine("========================================");
+				Console.WriteLine();
 			}
 
 			return result;
@@ -136,48 +169,57 @@ namespace LuckyTickets
 
 		/// <summary>
 		/// Метод тестирования разрабатываемого метода решения задачи. 
-		/// Да, тесты надо писать по-другому. 
-		/// Да, в этом методе я не следую принципу DRY.
+		/// Да, тесты надо писать по-другому.
 		/// </summary>
 		/// <returns>Успешно ли прошёл тест.</returns>
-		private static bool TestProcessingMethod(bool useLog)
+		private static bool TestProcessingMethod(bool useMultiThreadTest, bool useSingleThreadTest, bool useLog)
 		{
-			var count1 = TestGetLuckyTicketsCount(usingDigits: new List<char>() { '0', '1' }, maxRank: 7, checkingRanks1: new[] { 0, 1 }, checkingRanks2: new[] { 2, 3 }, useLog: useLog);
-			if (useLog)
-				Console.WriteLine();
-
-			var count2 = GetLuckyTicketsCount(usingDigits: new List<char>() { '0', '1' }, maxRank: 7, checkingRanks: 2, useLog: useLog);
-			if (useLog)
-				Console.WriteLine();
-
-			if (count1 != count2)
+			if (!useMultiThreadTest && !useSingleThreadTest)
 			{
 				if (useLog)
-					Console.WriteLine("Тест №1 разрабатываемого метода ПРОВАЛЕН.");
+					Console.WriteLine("Для тестирования требуется включить хотя бы один из методов проверки.");
 
 				return false;
 			}
 
-			count1 = TestGetLuckyTicketsCount(usingDigits: new List<char>() { '0', '1', '2' }, maxRank: 7, checkingRanks1: new[] { 0, 1, 2 }, checkingRanks2: new[] { 4, 5, 6 }, useLog: useLog);
-			if (useLog)
-				Console.WriteLine();
-
-			count2 = GetLuckyTicketsCount(usingDigits: new List<char>() { '0', '1', '2' }, maxRank: 7, checkingRanks: 3, useLog: useLog);
-			if (useLog)
-				Console.WriteLine();
-
-			if (count1 != count2)
+			bool test(IReadOnlyList<char> usingDigits, byte digitsCount, byte checkingRanks)
 			{
 				if (useLog)
-					Console.WriteLine("Тест №2 разрабатываемого метода ПРОВАЛЕН.");
+					Console.WriteLine($"Тестируем разрабатываемый метод с параметрами ({usingDigits.Count}, {digitsCount}, {checkingRanks}):");
 
-				return false;
+				var myMethodResult = GetLuckyTicketsCount(usingDigits, digitsCount, checkingRanks, useLog: false);
+				if (useLog)
+					Console.WriteLine($"Разрабатываемый метод вернул результат: {myMethodResult}");
+
+				var testMultiThreadMethodResult = useMultiThreadTest ? TestMultiThreadGetLuckyTicketsCount(usingDigits, digitsCount, checkingRanks, useLog) : - 1L;
+				
+				var testSingleThreadMethodResult = useSingleThreadTest ? TestSingleThreadGetLuckyTicketsCount(usingDigits, digitsCount, checkingRanks, useLog) : -1L;
+
+				if ((useMultiThreadTest && testMultiThreadMethodResult != myMethodResult) || (useMultiThreadTest && useSingleThreadTest && testMultiThreadMethodResult != testSingleThreadMethodResult) || (useSingleThreadTest && myMethodResult != testSingleThreadMethodResult))
+				{
+					if (useLog)
+						Console.WriteLine($"Тест ({usingDigits.Count}, {digitsCount}, {checkingRanks}) разрабатываемого метода ПРОВАЛЕН. [Тестируемый метод: {myMethodResult} | Многопоточный перебор: {testMultiThreadMethodResult} | Однопоточный перебор: {testSingleThreadMethodResult}]");
+
+					return false;
+				}
+
+				if (useLog)
+				{
+					Console.WriteLine($"Тест ({usingDigits.Count}, {digitsCount}, {checkingRanks}) разрабатываемого метода успешно пройден. [Результат: {myMethodResult}]");
+					Console.WriteLine();
+				}
+
+				return true;
 			}
 
-			if (useLog)
-				Console.WriteLine("Тесты разрабатываемого метода пройдены успешно.");
-
-			return true;
+			return
+				test(usingDigits: new List<char>() { '0', '1' }, digitsCount: 9, checkingRanks: 4) &&
+				test(usingDigits: new List<char>() { '0', '1', '2' }, digitsCount: 7, checkingRanks: 3) &&
+				test(usingDigits: new List<char>() { '0', '1', '2' }, digitsCount: 9, checkingRanks: 4) &&
+				test(usingDigits: new List<char>() { '0', '1', '2' }, digitsCount: 13, checkingRanks: 6) &&
+				test(usingDigits: Number13.Digits, digitsCount: 5, checkingRanks: 2) &&
+				test(usingDigits: Number13.Digits, digitsCount: 7, checkingRanks: 3) &&
+				test(usingDigits: Number13.Digits, digitsCount: 9, checkingRanks: 4);
 		}
 
 		/// <summary>
@@ -185,36 +227,84 @@ namespace LuckyTickets
 		/// Да, тесты надо писать по-другому. 
 		/// </summary>
 		/// <param name="usingDigits">Используемые цифры в системе счисления.</param>
-		/// <param name="maxRank">Количество цифр в проверяемых числах.</param>
-		/// <param name="checkingRanks1">Массив проверяемых 'порядков' (цифр) №1.</param>
-		/// <param name="checkingRanks2">Массив проверяемых 'порядков' (цифр) №2.</param>
+		/// <param name="digitsCount">Количество цифр в проверяемых числах.</param>
+		/// <param name="checkingRanks">Количество проверяемых 'разрядов' (цифр) слева и справа.</param>
 		/// <param name="useLog">Использовать ли вывод в консоль для логов.</param>
 		/// <returns>Результат решения задачи.</returns>
-		private static long TestGetLuckyTicketsCount(IReadOnlyList<char> usingDigits, int maxRank, int[] checkingRanks1, int[] checkingRanks2, bool useLog)
+		private static long TestSingleThreadGetLuckyTicketsCount(IReadOnlyList<char> usingDigits, int digitsCount, byte checkingRanks, bool useLog)
 		{
-			var minNumberIncl = 0;
-			var maxNumberNotIncl = new NumberX(new string(usingDigits[usingDigits.Count - 1], maxRank), usingDigits).ToInteger() + 1;
-			NumberX baseXNumber;
+			if (useLog)
+				Console.WriteLine($"Тестируем однопоточным перебором с параметрами ({usingDigits.Count}, {digitsCount}, {checkingRanks})...");
+
 			var luckyTicketCount = 0;
 
-			var luckyTickets = new List<string>();
-
-			for (var base10Number = minNumberIncl; base10Number < maxNumberNotIncl; base10Number++)
+			var numberX = new NumberX(0, usingDigits);
+			var maxNumberX = new NumberX(new string(usingDigits[usingDigits.Count - 1], digitsCount), usingDigits);
+			maxNumberX.IncrementAbs();
+			while (numberX != maxNumberX)
 			{
-				baseXNumber = new NumberX(base10Number, usingDigits);
-				if (checkingRanks1.Sum(rank => baseXNumber.ToInteger(rank)) == checkingRanks2.Sum(rank => baseXNumber.ToInteger(rank)))
+				var delta = 0;
+				for (var rank = 0; rank < checkingRanks; rank++)
 				{
-					var baseXNumberStr = baseXNumber.ToString();
-					luckyTickets.Add(baseXNumberStr);
-					//Console.WriteLine($"Найдено красивое число: {baseXNumberStr}");
+					delta += numberX.ToInteger(rank) - numberX.ToInteger(digitsCount - 1 - rank);
+				}
+
+				if (delta == 0)
+				{
 					luckyTicketCount++;
 				}
+
+				numberX.IncrementAbs();
 			}
 
 			if (useLog)
 			{
-				Console.WriteLine($"[Тестовый метод] Всего найдено красивых {maxRank}-тизначных чисел в {usingDigits.Count}-ричной системе счисления: {luckyTicketCount}");
-				Console.WriteLine();
+				Console.WriteLine($"[Тестовый однопоточный перебор] Результат: {luckyTicketCount}");
+				//Console.WriteLine();
+			}
+
+			return luckyTicketCount;
+		}
+
+		/// <summary>
+		/// Метод поиска решения задачи перебором. Используется для тестирования разрабатываемого метода. 
+		/// Да, тесты надо писать по-другому. 
+		/// </summary>
+		/// <param name="usingDigits">Используемые цифры в системе счисления.</param>
+		/// <param name="digitsCount">Количество цифр в проверяемых числах.</param>
+		/// <param name="checkingRanks">Количество проверяемых 'разрядов' (цифр) слева и справа.</param>
+		/// <param name="useLog">Использовать ли вывод в консоль для логов.</param>
+		/// <returns>Результат решения задачи.</returns>
+		private static long TestMultiThreadGetLuckyTicketsCount(IReadOnlyList<char> usingDigits, int digitsCount, byte checkingRanks, bool useLog)
+		{
+			if (useLog)
+				Console.WriteLine($"Тестируем многопоточным перебором с параметрами ({usingDigits.Count}, {digitsCount}, {checkingRanks})...");
+
+			var minNumberIncl = 0L;
+			var maxNumberNotIncl = new NumberX(new string(usingDigits[usingDigits.Count - 1], digitsCount), usingDigits).ToInteger() + 1;
+			if (maxNumberNotIncl < 0)
+				throw new Exception();
+
+			var luckyTicketCount = 0;
+
+			var task = Parallel.For(minNumberIncl, maxNumberNotIncl, base10Number =>
+			{
+				var baseXNumber = new NumberX(base10Number, usingDigits);
+				var delta = 0;
+				for (var rank = 0; rank < checkingRanks; rank++)
+				{
+					delta += baseXNumber.ToInteger(rank) - baseXNumber.ToInteger(digitsCount - 1 - rank);
+				}
+
+				if (delta == 0)
+				{
+					Interlocked.Increment(ref luckyTicketCount);
+				}
+			});
+
+			if (useLog)
+			{
+				Console.WriteLine($"[Тестовый многопоточный перебор] Результат: {luckyTicketCount}");
 			}
 
 			return luckyTicketCount;
@@ -255,6 +345,43 @@ namespace LuckyTickets
 			if (useLog)
 			{
 				Console.WriteLine("Тест конвертации чисел успешно пройден.");
+				Console.WriteLine();
+			}
+
+			return true;
+		}
+
+		/// <summary>
+		/// Метод проверки корректности вычисления суммы цифр чисел в 13-ричной системе счисления. 
+		/// Да, тесты надо писать по-другому. 
+		/// </summary>
+		/// <param name="useLog">Использовать ли вывод в консоль для логов.</param>
+		/// <returns>Успешно ли прошёл тест.</returns>
+		private static bool TestDigitsSum(bool useLog)
+		{
+			if 
+			( 
+				new Number13("0").GetDigitsSum() != 0 
+				|| new Number13("A").GetDigitsSum() != 10
+				|| new Number13("C").GetDigitsSum() != 12
+				|| new Number13("10").GetDigitsSum() != 1
+				|| new Number13("1A").GetDigitsSum() != 11
+				|| new Number13("AA").GetDigitsSum() != 20
+				|| new Number13("CC").GetDigitsSum() != 24
+				|| new Number13("CCC").GetDigitsSum() != 36
+				|| new Number13("CCC1").GetDigitsSum() != 37
+				|| new Number13("1CCC1").GetDigitsSum() != 38
+			)
+			{
+				if (useLog)
+					Console.WriteLine($"Тест вычисления суммы цифр ПРОВАЛЕН.");
+
+				return false;
+			}
+
+			if (useLog)
+			{
+				Console.WriteLine("Тест суммы цифр успешно пройден.");
 				Console.WriteLine();
 			}
 
